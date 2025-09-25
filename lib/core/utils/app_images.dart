8caa91cc61dd2aebf099af91abbbdeca8dfdb6d8
mano299 <mano299ahmed1@gmail.com>
2025-09-25 10:01:10 +0300

    create assets path file

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesOnBoardingBg
+  /// assets/images/onBoarding_bg.svg
+  static const String assetsImagesOnBoardingBg = "assets/images/onBoarding_bg.svg";
+
+  /// Assets for assetsImagesOnBoardingPhoto1
+  /// assets/images/on_boarding_photo1.svg
+  static const String assetsImagesOnBoardingPhoto1 = "assets/images/on_boarding_photo1.svg";
+
+  /// Assets for assetsImagesOnBoardingPhoto2
+  /// assets/images/on_boarding_photo2.svg
+  static const String assetsImagesOnBoardingPhoto2 = "assets/images/on_boarding_photo2.svg";
+
+  /// Assets for assetsImagesOnBoardingPhoto3
+  /// assets/images/on_boarding_photo3.svg
+  static const String assetsImagesOnBoardingPhoto3 = "assets/images/on_boarding_photo3.svg";
+
+  /// Assets for assetsImagesSplashBg
+  /// assets/images/splash_bg.svg
+  static const String assetsImagesSplashBg = "assets/images/splash_bg.svg";
+
+  /// Assets for assetsImagesSplashLogo
+  /// assets/images/splash_logo.svg
+  static const String assetsImagesSplashLogo = "assets/images/splash_logo.svg";
+}
+
---
+++ lib/features/splash_screen/splash_screen_body.dart
@@ -0,0 +1,16 @@
+import 'package:doctor_app/core/utils/app_images.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreenBody extends StatelessWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SvgPicture.asset(Assets.assetsImagesOnBoardingBg),
+      ],
+    );
+  }
+}
