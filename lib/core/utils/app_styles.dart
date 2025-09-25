af8b4c6fdb4b09981089c612619f79e17dbe4828
mano299 <mano299ahmed1@gmail.com>
2025-09-25 10:41:23 +0300

    create animation for splash screen

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle styleBold25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Rubik',
+    color: Colors.black,
+  );
+}
--- lib/features/splash_screen/splash_screen_body.dart
+++ lib/features/splash_screen/splash_screen_body.dart
@@ -1,7 +1,29 @@
+import 'package:doctor_app/core/utils/app_images.dart';
+import 'package:doctor_app/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreenBody extends StatelessWidget {
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody>
+    with TickerProviderStateMixin {
+  late AnimationController textAnimationController;
+  late AnimationController logoAnimationController;
+  late Animation<Offset> textSlidingAnimation;
+  late Animation<Offset> logoSlidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initSlidingAnimation();
+    // homeViewNavigate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +35,52 @@ class SplashScreenBody extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SlideTransition(
+                    position: logoSlidingAnimation,
+                    child: SvgPicture.asset(Assets.assetsImagesSplashLogo)),
+                SizedBox(height: 12),
+                SlideTransition(
+                  position: textSlidingAnimation,
+                  child: Text(
+                    'Doctor Hunt',
+                    style: AppStyles.styleBold25,
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
     );
+  }
+
+  void initSlidingAnimation() {
+    textAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    logoAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    textSlidingAnimation = Tween<Offset>(
+      begin: Offset(0, 3),
+      end: Offset.zero,
+    ).animate(textAnimationController);
+
+    logoSlidingAnimation = Tween<Offset>(
+      begin: Offset(0, -3),
+      end: Offset.zero,
+    ).animate(logoAnimationController);
+
+    logoAnimationController.forward();
+    textAnimationController.forward();
   }
 }
