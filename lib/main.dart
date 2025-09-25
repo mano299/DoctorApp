da7c115c089843eb9f8b0e478ea6260c77bc7465
mano299 <mano299ahmed1@gmail.com>
2025-09-25 09:42:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorApp());
+}
+
+class DoctorApp extends StatelessWidget {
+  const DoctorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doctor App',
+    );
+  }
+}
