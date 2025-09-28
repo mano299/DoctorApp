eea119efd7f3f104d9c4f257146e2eb7e7b39ab5
mano299 <mano299ahmed1@gmail.com>
2025-09-28 04:16:58 +0300

    find doctor view body part 1

--- lib/features/find_doctors/presentation/widgets/find_doctors_view_body.dart
+++ lib/features/find_doctors/presentation/widgets/find_doctors_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_app/core/utils/app_images.dart';
 import 'package:doctor_app/core/utils/app_styles.dart';
 import 'package:doctor_app/core/widgets/pop_button.dart';
+import 'package:doctor_app/features/home/presentation/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class FindDoctorsViewBody extends StatelessWidget {
@@ -16,15 +17,28 @@ class FindDoctorsViewBody extends StatelessWidget {
           fit: BoxFit.fill,
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 50.0, left: 20),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              PopButton(),
-              SizedBox(width: 8),
-              Text(
-                'Find Doctors',
-                style: AppStyles.styleRegular18,
+          padding: const EdgeInsets.only(top: 60.0, left: 20, right: 20),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        PopButton(),
+                        SizedBox(width: 19),
+                        Text(
+                          'Find Doctors',
+                          style: AppStyles.styleRegular18,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 34),
+                    CustomTextField(hintText: 'Dentist'),
+                    SizedBox(height: 24),
+                  ],
+                ),
               ),
             ],
           ),
